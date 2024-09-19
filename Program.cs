ad7868af8cea1a1a7c2beb5fec118aef6d8cf98d
Elvira L <elvira.liljestrand@hotmail.com>
2024-09-19 12:48:08 +0200

    Maktgalen

--- Program.cs
+++ Program.cs
@@ -1,2 +1,4 @@
 ﻿Console.WriteLine("Rickard är en super tönt...");
-Console.WriteLine("en super tönt.>");
+Console.WriteLine("en super tönt.");
+Console.WriteLine("Tur att Max är här.");
+Console.WriteLine("Nu är jag ensam :(");

8e3f5891791c600daa52396db21fa46562fd9592
Elvira L <elvira.liljestrand@hotmail.com>
2024-09-19 13:25:49 +0200

    Hybris

--- Program.cs
+++ Program.cs
@@ -1,5 +1,5 @@
 ﻿Console.WriteLine("Rickard är en super tönt...");
 Console.WriteLine("en super tönt.");
-Console.WriteLine("Tur att Max är här.");
+Console.WriteLine("Max har lämnat mig.");
 Console.WriteLine("Nu är jag ensam :(");
 Console.WriteLine("lonely, i am so lonely");
